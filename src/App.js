dead8397be3749551f34f946036f183c1add5dbd
SayedRaju1 <sayedraju21@gmail.com>
2022-03-04 18:07:40 +0600

    done with responsive

--- src/App.js
+++ src/App.js
@@ -5,26 +5,27 @@ function App() {
   return (
     <div className="main_div">
       <div className="div_second">
-        <div className="div_second_text1">
+        <div>
           <p className="div_second_text1_para lazyDog">MEMS</p>
-        </div>
-        <div className="div_second_text2 dm">
           <p className="div_second_text2_para">restoring earth's memory</p>
         </div>
+
         <div className="div_second_text3">
-          <p className="div_second_text3_para">Coming soon!</p>
+          Coming soon!
         </div>
       </div>
 
       <div className="div_images">
-        <img className="image-1" src="/image/man_pencil.png" />
-        <img className="image-2" src="/image/base.png" />
+        <img className="image-1" alt="mems" src="/image/man_pencil.png" />
+        <img className="image-2" alt="mems2" src="/image/Image_33.png" />
+        <img className="image-3" alt="mems" src="/image/base.png" />
+
       </div>
     </div>
   );
 }
 
 // 54
-// group_21
+// group_21 Image_33.png
 
 export default App;
