77c5ca464292af8f92f7b18fc49a2c507cb074aa
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-03-28 01:30:15 +0600

    svg added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import "./fonts/Lazydog.otf"
+import "./fonts/Lazydog.otf";
 
 function App() {
   return (
@@ -8,15 +8,15 @@ function App() {
       <div className="div_second">
         <div className="text-div">
           <p className="div_second_text1_para lazyDog">MEMS</p>
-          <p className="div_second_text2_para">6000+ mems on the ethereum blockchain</p>
+          <p className="div_second_text2_para">
+            6000+ mems on the ethereum blockchain
+          </p>
         </div>
 
-        <div className="div_second_text3">
-          Coming soon
-        </div>
+        <div className="div_second_text3">Coming soon</div>
       </div>
       <div className="image_div">
-        <img className="the_image" alt="mems" src="/image/image_61.png" />
+        <img className="the_image" alt="mems" src="/image/meme-landing.svg" />
       </div>
 
       <div className="social_div">
@@ -32,7 +32,6 @@ function App() {
         <a href="https://www.google.com" target="_blank" rel="noreferrer">
           <img src="/image/opensea.png" alt="opensea" className="social_logo" />
         </a> */}
-
       </div>
     </div>
   );
